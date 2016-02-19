ce883608be6d9a3b5394191d7a63deb2800b3bff
zhenglianfu <15067134439@163.com>
2016-02-19 17:57:51 +0800

    remove useless variables

--- jsLoader.js
+++ jsLoader.js
@@ -40,8 +40,7 @@
         paths = {},
         moduleCache = {},
         manifestCache = {},
-        moduleManifest = [],
-        core_slice = [].slice;
+        moduleManifest = [];
 
     /* util functions */
     var rtrim = /^\s+|\s+$/g;
@@ -89,14 +88,13 @@
              **/
             /**
              * #  bug : wait for parse js file completed, then load the next js file  #
-             * js文件解析完成之后再加载下一个(js引擎不够快，文件过大导致)
+             * js文件解析完成之后再加载下一个
              * */
             var i = 0, len = modules.length, data = {}, token;
-            // reverse it, as a stack LIFO
-            modules = modules.reverse();
             errors = errors || [];
             fn = fn || Util.foo;
-            for (; i < len; i ++) {
+            // order as a stack FILO
+            for (i = len - 1; i >= 0 ; i --) {
                 token = modules[i];
                 if (paths[token].url) {
                     fn = function (token, g) {
@@ -204,13 +202,17 @@
         getDependModules : function(module, imports){
             var depends = Util.getManifeset(module).depend, i = 0, len = depends.length, item;
             imports = imports || [];
+            // duplicate
+            if (imports.indexOf(module) >= 0) {
+            	return imports;
+            }
             for (; i < len; i ++) {
                 item = moduleManifest[depends[i]];
                 if (imports.indexOf(item.name) < 0) {
                     if (item.depend.length) {
                         imports = Util.getDependModules(item.name, imports);
                     }
-                    imports.push(item.name);
+                    imports.indexOf(item.name) < 0 && imports.push(item.name);
                 }
             }
             return imports;
@@ -278,7 +280,7 @@
             for (; i < ns_len; i++) {
                 m = Util.trim(ns[i]);
                 if (paths[m]) {
-                    requires = requires.concat(Util.getDependModules(m));
+                    requires = Util.getDependModules(m, requires);
                     requires.push(m);
                 } else {
                     errors.push({
@@ -380,6 +382,11 @@
                 moduleCache = {};
             }
         },
+        setModuleValue: function(moduleName, value){
+            if (value != null) {
+                moduleCache[moduleName] = value;    
+            }
+        },
         mainMethod : function(){
             // configuration if existed
             if (_config_) {
@@ -440,7 +447,8 @@
     // interface PManager finished
     win.PManager = {
         load : Util.loadModule,
-        add : Util.addModules
+        add : Util.addModules,
+        setModuleValue: Util.setModuleValue
     };
     // main
     Util.mainMethod();
